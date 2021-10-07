79ec63cb0e06726d7e20e74297940e919988a736
sigfredo14 <sigfredocamas@gmail.com>
2021-10-07 16:07:44 -0500

    primer comit

---
+++ src/main/java/com/system/model/Persona.java
@@ -0,0 +1,46 @@
+package com.system.model;
+
+public class Persona {
+	private int idPersona;
+	private String nombre;
+	private String apellido;
+	
+	
+	public Persona() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public int getIdPersona() {
+		return idPersona;
+	}
+
+
+	public void setIdPersona(int idPersona) {
+		this.idPersona = idPersona;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getApellido() {
+		return apellido;
+	}
+
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	
+	
+
+}
